97fe4878e078923292d19772dbb598bdae91e58c
JonasLannoo <jonas.lannoo@gmail.com>
2025-05-19 21:16:40 +0200

    added second channel and recording method to lanxi class

--- HelpFunctions/lanxi.py
+++ HelpFunctions/lanxi.py
@@ -1,5 +1,9 @@
 import requests
 from.import utility as utility
+import numpy as np
+import socket
+from openapi.openapi_header import *
+from openapi.openapi_stream import *
 
 class LanXI:
     def __init__(self, ip):
@@ -47,6 +51,12 @@ class LanXI:
                 self.setup["channels"][channel_nr]["transducer"] = self.channels[channel_nr]
                 self.setup["channels"][channel_nr]["enabled"] = True
                 self.setup["channels"][channel_nr]["ccld"] = self.channels[channel_nr]["requiresCcld"]
+        # Configure channel 2 (index 1) as analog force channel with 10 Vpeak range
+        if len(self.setup["channels"]) > 1:
+            self.setup["channels"][1]["transducer"] = None
+            self.setup["channels"][1]["enabled"] = True
+            self.setup["channels"][1]["ccld"] = False
+            self.setup["channels"][1]["range"] = "10 Vpeak"  # Set the correct range for force sensor
         # Remove None channels
         self.channels = list(filter(lambda x : x != None, self.channels))
         if not any(self.channels):
@@ -73,3 +83,60 @@ class LanXI:
         supported_sample_rates = module_info["supportedSampleRates"]
         # Find the sample rate with the minimum difference to bandwidth * 2
         self.sample_rate = min(supported_sample_rates, key = lambda x:abs(x - bandwidth * 2))
+
+    def SampleChannels(self, duration):
+        """
+        Sample both channels for the given duration (in seconds).
+        Returns:
+            time_axis: np.ndarray of time values
+            data: np.ndarray shape (2, N) where N is the number of samples
+        """
+        sample_rate = self.sample_rate
+        num_samples = int(sample_rate * duration)
+        arrays = [[], []]  # For channel 1 and 2
+        interpretations = [{},{},{},{},{},{}]
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.ip, self.inputport))
+            total_samples = 0
+            while total_samples < num_samples:
+                # Get header
+                data = s.recv(28)
+                wstream = OpenapiHeader.from_bytes(data)
+                content_length = wstream.content_length + 28
+                # Get rest of package
+                while len(data) < content_length:
+                    packet = s.recv(content_length - len(data))
+                    data += packet
+                # Parse package
+                package = OpenapiStream.from_bytes(data)
+                if package.header.message_type == OpenapiStream.Header.EMessageType.e_interpretation:
+                    for interpretation in package.content.interpretations:
+                        interpretations[interpretation.signal_id - 1][interpretation.descriptor_type] = interpretation.value
+                if package.header.message_type == OpenapiStream.Header.EMessageType.e_signal_data:
+                    for signal in package.content.signals:
+                        if signal is not None and (signal.signal_id == 1 or signal.signal_id == 2):
+                            scale_factor = interpretations[signal.signal_id - 1].get(
+                                OpenapiStream.Interpretation.EDescriptorType.scale_factor, 1.0
+                            )
+                            values = np.array([x.calc_value for x in signal.values])
+                            # Convert to volts (divide by 2^23 and multiply by scale factor)
+                            values = (values * scale_factor) / (2 ** 23)
+                            arrays[signal.signal_id - 1].extend(values)
+                            if signal.signal_id == 1:
+                                total_samples = len(arrays[0])
+            # Stop measurement
+            import requests
+            requests.put(self.host + "/rest/rec/measurements/stop")
+            s.close()
+        import requests
+        requests.put(self.host + "/rest/rec/finish")
+        requests.put(self.host + "/rest/rec/close")
+
+        # Truncate to the same length and to num_samples
+        min_len = min(len(arrays[0]), len(arrays[1]), num_samples)
+        ch1 = np.array(arrays[0][:min_len])
+        ch2 = np.array(arrays[1][:min_len])
+        time_axis = np.linspace(0, min_len / sample_rate, min_len, endpoint=False)
+        data = np.vstack([ch1, ch2])
+        return time_axis, data
